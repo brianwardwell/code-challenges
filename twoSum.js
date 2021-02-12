08f49f5bece12de77a2bf4037de1c1921ae1478b
Brian Wardwell <bwardwell79@gmail.com>
2021-02-12 15:52:07 -0800

    fixed for all test cases

--- twoSum.js
+++ twoSum.js
@@ -12,8 +12,11 @@
 // Only one valid answer exists.
 
 var twoSum = function(nums, target) {
+    
     for (i=0; i < nums.length; i++) {
-        for (j=1; j < nums.length - 1; j++) {
+        
+        for (j=i + 1; j < nums.length; j++) {
+        
             if (nums[i] + nums[j] === target) {
                 return [i, j]
             }
