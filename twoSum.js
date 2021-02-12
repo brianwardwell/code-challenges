d10ffe819bcdf52368e40ee447fe3a9d595c87cf
Brian Wardwell <bwardwell79@gmail.com>
2021-02-12 15:33:34 -0800

    completed twoSum LeetCode

---
+++ twoSum.js
@@ -0,0 +1,22 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Constraints:
+
+// 2 <= nums.length <= 103
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+var twoSum = function(nums, target) {
+    for (i=0; i < nums.length; i++) {
+        for (j=1; j < nums.length - 1; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+};
